3548c501f6d60547ecd80e9c25005e0c889afe19
mobashirat <mirzatazneen@gmail.com>
2021-09-26 23:07:57 +0600

    first commit

--- src/components/Galleries/Galleries.js
+++ src/components/Galleries/Galleries.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 import Cart from '../Cart/Cart';
 import Painting from '../Painting/Painting';
 
